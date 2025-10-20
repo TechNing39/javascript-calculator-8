4c472a9cf2c3e315ff23e78f40d8101013a4a555
TechNing39 <leeyujineee@gmail.com>
2025-10-20 21:39:24 +0900

    feat: 커스텀 구분자 기능 구현

--- src/App.js
+++ src/App.js
@@ -3,8 +3,18 @@ class App {
     if(!text){
       return 0;
     }
-    const numbers=text.split(/[,:]/).map(Number);
-    return numbers.reduce((sum,num)=>sum+num,0);
+    let delimiter=/[,\:]/;
+    let numberString=text;
+
+    const customDelimiter=text.match(/^\/\/(.)\n(.*)/s);
+    if(customDelimiter){
+      delimiter=new RegExp(customDelimiter[1]);
+      numberString=customDelimiter[2];
+    }
+
+    const numbers = numberString.split(delimiter).map(Number);
+    const sum=numbers.reduce((sum,num)=>sum+num,0);
+    return sum;
   }
   async run() {}
 }
